b0f4a4e9ba553c69404bad1cb0d5d2fbe15e491c
DanielNanto <nanto.daniel@gmail.com>
2024-06-16 15:59:07 -0500

    Converted unsigned_delta() to an inline funcction.

--- src/utils.c
+++ src/utils.c
@@ -4,8 +4,7 @@
 /// \param value_a One of the uint32_t values to be evaluated.
 /// \param value_b The other uint32_t value to be evaluated.
 /// \returns The unsigned delta of the two uint32_t values.
-uint32_t unsigned_delta(uint32_t value_a, uint32_t value_b)
+inline uint32_t unsigned_delta(uint32_t value_a, uint32_t value_b)
 {
-  uint32_t delta = (value_a > value_b) ? value_a - value_b : value_b - value_a;
-  return delta;
+  return (value_a > value_b) ? value_a - value_b : value_b - value_a;
 }
